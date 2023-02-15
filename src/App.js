842b9158e64a36d29be8fc7ff3739c42c1d8c257
Pranav Sen <pranav.sen2019@vitbhopal.ac.in>
2023-02-15 17:00:40 +0530

    add api

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// https://api.openweathermap.org/data/2.5/weather?q=sehore&units=metric&appid=4bd96f554812695592e446cd50db6a9f
+import Temp from './weather/temp';
+function App() {
+  return (
+    <>
+      <Temp/>
+    </>
+  );
+}
+
+export default App;
